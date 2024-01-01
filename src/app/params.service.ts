87c1e84c7571dc5eaf905f74838c6ee9239e6417
alaeddine.bouhajja <alaeddine.bouhajja@tunav.com>
2024-01-01 13:54:02 +0100

    getting token from coockies

--- src/app/params.service.ts
+++ src/app/params.service.ts
@@ -13,13 +13,33 @@ export class ParamsService {
 
   getParams(): Observable<any> {
     const csrfToken = localStorage.getItem('token');
+    const csrfTokenCockies = this.getCookie('csrftoken');
+    console.log('csrfToken:', csrfToken);
+    console.log('csrfTokenCockies:', csrfTokenCockies);
+    
     const httpOptions = {
       headers: new HttpHeaders({
         'Content-Type': 'application/json',
-        'X-CSRFToken': csrfToken || '',
-      })
+        'X-CSRFToken': '' + csrfTokenCockies,
+        'Authorization': 'Bearer ' + csrfTokenCockies,
+      }),
+      // withCredentials: true,
     };
 
-    return this.http.post<any>(this.paramsUrl, {}, httpOptions);
+    return this.http.post<any>(this.paramsUrl, {}, httpOptions, );
+  }
+  
+  private getCookie(name: string): string {
+      const value = `; ${document.cookie}`;
+      const parts = value.split(`; ${name}=`);
+      
+      if (parts.length === 2) {
+          const cookieValue = parts.pop()?.split(';').shift();
+          if (cookieValue) {
+              return cookieValue;
+          }
+      }
+
+      return '';
   }
 }
